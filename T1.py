0fe82f79f8a9fac2b6932f03ca6f30558434b440
OscarOlano <oscarandre0406@gmail.com>
2025-05-02 19:10:30 -0500

    T1.py

--- T1.py
+++ T1.py
@@ -8,29 +8,25 @@ class Equipo:
         self.setGanados = 0
 
     def puntos(self):
-        # Genera un número aleatorio de puntos entre 10 y 28
         return random.randint(10, 28)
 
     def puntos_extras(self):
-        # Genera un número aleatorio de puntos extra entre 0 y 6
         return random.randint(0, 6)
 
     def registrar_set(self, equipo_ganador):
-        # Incrementa el set ganado de un equipo
         if equipo_ganador == 1:
             self.setGanados += 1
         elif equipo_ganador == 2:
             self.setGanados += 1
 
         if self.setGanados == 3:
-            return True  # El equipo ha ganado el partido
+            return True 
         return False
 
     def jugar_partido(self, equipo2):
         set_equipo1 = 0
         set_equipo2 = 0
 
-        # Simulamos los sets para que un equipo gane el partido
         while True:
             puntos_equipo1 = self.puntos()
             puntos_equipo2 = equipo2.puntos()
@@ -47,20 +43,20 @@ class Equipo:
 
             if puntos_equipo1 > puntos_equipo2:
                 set_equipo1 += 1
-                print(f"{self.nombre} gana el set")
+                print(f"{self.nombre} gano el set")
             else:
                 set_equipo2 += 1
-                print(f"{equipo2.nombre} gana el set")
+                print(f"{equipo2.nombre} gano el set")
 
             if set_equipo1 == 3:
                 self.registrar_set(1)
                 equipo2.registrar_set(2)
-                print(f"{self.nombre} gana el partido")
+                print(f"{self.nombre} gano el partido")
                 return 1 
             elif set_equipo2 == 3:
                 self.registrar_set(2)
                 equipo2.registrar_set(1)
-                print(f"{equipo2.nombre} gana el partido")
+                print(f"{equipo2.nombre} gano el partido")
                 return 2  
 
     def mostrar_resultado(self, equipo2):
@@ -69,21 +65,17 @@ class Equipo:
         print(f"{equipo2.nombre} - Partidos Ganados: {equipo2.partidosGanados}, Partidos Perdidos: {equipo2.partidosPerdidos}")
 
 
-# Crear los equipos
 nombre_equipo1 = input("Ingrese el nombre del primer equipo: ")
 nombre_equipo2 = input("Ingrese el nombre del segundo equipo: ")
 
 equipo1 = Equipo(nombre_equipo1)
 equipo2 = Equipo(nombre_equipo2)
 
-# Jugar el número de partidos indicado
 partidos_a_jugar = int(input("Ingrese el número de partidos por jugar: "))
 
 for _ in range(partidos_a_jugar):
-    print(f"\nIniciando un nuevo partido entre {equipo1.nombre} y {equipo2.nombre}")
     ganador = equipo1.jugar_partido(equipo2)
     
-    # Se registra al ganador del partido
     if ganador == 1:
         equipo1.partidosGanados += 1
         equipo2.partidosPerdidos += 1
